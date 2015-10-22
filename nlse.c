155ca7e93b72d32511b8cca0207941feb2c0e9fc
Omar Ashour <omar.ashour22@gmail.com>
2015-10-22 23:10:17 +0300

    Restructure code and output
    - Fully restructured code
    - Commented most parts
    - Improved CLI by adding more input parameters
    - Added option to choose what to print and how much spacing

--- nlse.c
+++ nlse.c
@@ -2,95 +2,123 @@
 
 int main(int argc, char *argv[])
 {
-    //const bool print_results = 1;
-    //const bool print_spectrum = 0;
-    //const int fourier_sampling = 100;
-    //const int result_sampling;
+    // Simulation parameters
+    double dt, A1, q, tm; 
+    int order, nx, spectrum_sampling, psi_sampling; 
+    char temp1, temp2;
+    char type;
 
-    // Grid parameters
-    const double dt, A1, q, tm; const int order, nx; const char type;
-    printf("Enter dt: ");
+    // Prepare simulation
+    // Basic grid parameters
+    printf("dt: ");                         // Grid temporal spacing          
     scanf("%lf", &dt);
-    printf("Enter tm: ");
+    printf("tm: ");                         // Maximum time
     scanf("%lf", &tm);
-    printf("Enter nx: ");
-    scanf("%d", &nx);
-    printf("Enter A1: "); 
+    printf("nx: ");                         // Number of Fourier modes
+    scanf("%d", &nx);                       
+    printf("A1: ");                         // Cosine amplitude 
     scanf("%lf", &A1);
-    printf("Enter q (0<q<0.5): ");
+    printf("a: ");                          // Breather parameter
     scanf("%lf", &q);
-    printf("Enter algorithm order (2, 4, 6, 8): ");
+
+    // Determine algorithm 
+    printf("Algorithm order: ");            // Pick algorithm
     scanf("%d", &order);
-    printf("Enter algorithm type (S for symplect, M for multiproduct): ");
-    scanf(" %c", &type);
-    printf("Do you want to print the results? [0/1]");
-    scanf("%d", &print_results);
-    if (print_results)
+    if(order != 2)
     {
-        printf("Choose sampling. 1 means print whole array, N prints one every N elements.");
-        scanf("%d", &result_sampling);
+        printf("Integrator type: ");        // Type of integrator
+        scanf(" %c", &type);
     }
-    printf("Do you want to print the spectrum? [0/1]");
-    scanf("%d", &print_spectrum);
-    if (print_results)
+    else
+        type = '-';
+
+    // Psi output 
+    printf("Print psi? ");                  // Print psi or not
+    scanf(" %c", &temp1);
+    const bool print_psi = (temp1 != 'n');
+    if (print_psi)
     {
-        printf("Choose sampling. 1 means print whole array, N prints one every N elements.");
-        scanf("%d", &fourier_sampling);
+        printf("Result sampling: ");       // If yes, how often
+        scanf("%d", &psi_sampling);
     }
-
-
-    const int nt = tm/dt;   			        // number of temporal nodes
-    const double l = M_PI/sqrt(1-2*q);		    // Spatial Period
-    const double dx = (l / nx);			        // spatial step size
-    const double Omega = 2*sqrt(1-2*q);
-    const double A0 = sqrt(1-2*A1*A1);
-    const int size = nt/fourier_sampling;
     
-    char paramf[20];
-    sprintf(paramf, "output/param.txt");
-    FILE *fp = fopen(paramf, "w");
+    // Spectrum output 
+    printf("Print spectrum? ");            // Print spectrum or not
+    scanf(" %c", &temp2);
+    const bool print_spectrum = (temp2 != 'n');
+    if (print_spectrum)
+    {
+        printf("Spectrum sampling: ");     // If yes, how often
+        scanf("%d", &spectrum_sampling);
+    }
+
+    printf("-----------------------------------.\n");
+    printf("Running Program.\n");
+    printf("-----------------------------------.\n");
+    // Derived parameters
+    int r_size, s_size;
+    const int nt = tm/dt;   			        // Number of temporal nodes
+    const double l = M_PI/sqrt(1-2*q);		    // Spatial period/twice box length
+    const double dx = (l / nx);			        // Spatial step size
+    const double Omega = 2*sqrt(1-2*q);         // Fundamental frequency
+    const double A0 = sqrt(1-2*A1*A1);          // Normalization factor
+    if (print_psi)
+        r_size = nt/psi_sampling;           // Total size of results array
+    if (print_spectrum)
+        s_size = nt/spectrum_sampling;           // Total size of spectrum array
+    
+    // Print parameter file
+    printf("Printing param.txt file.\n");
+    FILE *fp = fopen("output/param.txt", "w");
     fprintf(fp, "%.13f\n"
                 "%d\n"
                 "%.13f\n"
                 "%.13f\n"
                 "%.13f\n"
                 "%d\n"
-                "%c\n"
-                "%d\n", dt, nx, tm, A1, q, order, type, size);
+                "%c\n", dt, nx, tm, A1, q, order, type);
     fclose(fp);
 
     // Print basic info about simulation
-    printf("dt=%f\nnx=%d\ntm=%f\norder=%d\ntype=%c\nq=%f\n", 
-            dt, nx, tm, order, type, q);
+    printf("dt:    %.5f\n"
+           "nx:    %d\n"
+           "tm:    %.3f\n"
+           "A1:    %.1e\n"
+           "a:     %.8f\n"
+           "Order: %d\n"
+           "Type:  %c\n", dt, nx, tm, A1, q, order, type);
 
     // Allocate the arrays
+    printf("Allocating arrays.\n");
     double *psi_r, *psi_i, *spectrum;
     fftw_complex *psi_f;
     double *x = (double*)malloc(sizeof(double) * nx);
     double *k2 = (double*)malloc(sizeof(double) * nx);
+    double *k = (double*)malloc(sizeof(double) * nx);
     fftw_complex *psi = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * nx);
     if (print_spectrum)
     {
-        spectrum = (double*) malloc(sizeof(double) * size*nx);
-        psi_f = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * nx);
+        spectrum = (double*)malloc(sizeof(double) * nx*s_size);  // Full spectrum
+        psi_f = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nx); // Step FFT
     }
-    if (print_results)
+    if (print_psi)
     {
-        psi_r = (double*) malloc(sizeof(double) * nx * nt);
-        psi_i = (double*) malloc(sizeof(double) * nx * nt);
+        psi_r = (double*)malloc(sizeof(double) * nx*r_size);     // Real part
+        psi_i = (double*)malloc(sizeof(double) * nx*r_size);     // Imaginary part
     }
 
-    fftw_complex *psi1, *psi2, *psi3, *psi4;
     // Allocating extra arrays for high orders
-    if (order > 2)
+    printf("Allocating extra arrays.\n");
+    fftw_complex *psi1, *psi2, *psi3, *psi4;
+    if (order > 2)                              // Special arrays for order 4, 6, 8 
     {
-        psi1 = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * nx);
-        psi2 = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * nx);
+        psi1 = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nx);
+        psi2 = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nx);
     }
-    if (order > 4)
-        psi3 = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * nx);
-    if (order > 6)
-        psi4 = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * nx);
+    if (order > 4)                              // Special arrays for order 6, 8 
+        psi3 = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nx);
+    if (order > 6)                              // Special arrays for order 8  
+        psi4 = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nx);
 
     // Create transform plans
     printf("Creating plans.\n");
@@ -100,45 +128,52 @@ int main(int argc, char *argv[])
     if (print_spectrum)
         f0_plan = fftw_plan_dft_1d(nx, psi, psi_f, FFTW_FORWARD, FFTW_ESTIMATE);
 
-    // Create wave number. Flipped to make up for not shifting 0 frequency component to the middle.
-    printf("Setting up initial conditions.\n");
+    // Create wave number. '
+    //Flipped to force-shift 0 frequency component to the middle when in k-domain.
+    printf("Preparing wavenumber.\n");
     double dk=2*M_PI/nx/dx;
-    double *k = (double*)malloc(nx*sizeof(double));
     for(int i = nx/2; i >= 0; i--) 
         k[nx/2-i]=(nx/2-i)*dk;
     for(int i = nx/2+1; i < nx; i++)
 	    k[i]=(i-nx)*dk; 
 
     // Initial conditions
+    printf("Setting up initial conditions.\n");
     for (int i = 0; i < nx; i++)
     {
-        x[i] = (i-nx/2)*dx;
-	    k2[i] = k[i]*k[i];
-	    psi[i] = (A0+2*A1*cos(Omega*x[i])) + 0*I; 
+        x[i] = (i-nx/2)*dx;                         // x array
+	    k2[i] = k[i]*k[i];                          // Square wave number
+	    psi[i] = (A0+2*A1*cos(Omega*x[i])) + 0*I;   // Initial WF 
     }
    
     // Start time evolution
-    int h = 0;
+    int h = 0, m = 0;                                      // Spectrum/psi size 
     printf("Evolving time.\n");
     for (int i = 0; i < nt; i++)
     {
-        if (print_spectrum && (i % fourier_sampling == 0))
+        if (print_spectrum && (i % spectrum_sampling == 0))
         {
-            fftw_execute(f0_plan);
+            fftw_execute(f0_plan);                         // Get step's spectrum 
             for (int j = 0; j < nx; j++)
-                spectrum[ind(j, h)] = cabs(psi_f[j])/nx;
+            {
+                spectrum[ind(h,j)] = cabs(psi_f[j])/nx;   // Save spectrum
+                printf("h/j/ind: %d/%d/%d\n", h, j, ind(h, j));
+            }
             h++;
         }
      
-        if (print_results)
+        // Save results for printing
+        if (print_psi && (i % psi_sampling == 0))
         {
             for (int j = 0; j < nx; j++)
             {
-                psi_i[ind(i,j)] = cimag(psi[j]);
-                psi_r[ind(i,j)] = creal(psi[j]);
+                psi_r[ind(m,j)] = creal(psi[j]);
+                psi_i[ind(m,j)] = cimag(psi[j]);
             }	
+            m++;
         }
 
+        // Evolve one step in time, depending on algorithm
  	    if (order == 2)
 	        T2(psi, psi, nt, dt, k2, nx, forward, backward); 
         else if (order == 4 && type == 'M')
@@ -155,61 +190,59 @@ int main(int argc, char *argv[])
             T8_S(psi, nt, dt, k2, nx, forward, backward);
         else
         {
-            printf("Unknown order. Error.\n"); //Exit somehow
+            printf("Unknown order. Error.\n");
             return 1;
         }
     }
-    printf("%d/%d", size, h);
+    
     // Print output
-    if (print_results)
+    if (print_psi)
     {
         printf("Printing output.\n");
-        char im_f[20];
-        char r_f[20];
-        char x_f[20];
-        sprintf(im_f, "output/psi_i.bin");
-        sprintf(r_f, "output/psi_r.bin");
-        sprintf(x_f, "output/x.bin");
-        
-        fp=fopen(im_f, "wb");
-        fwrite(psi_i, sizeof(double), nx*nt, fp);
+        fp=fopen("output/psi_i.bin", "wb");
+        fwrite(psi_i, sizeof(double), nx*r_size, fp);
         fclose(fp);
-        fp=fopen(r_f, "wb");
-        fwrite(psi_r, sizeof(double), nx*nt, fp);
+        fp=fopen("output/psi_r.bin", "wb");
+        fwrite(psi_r, sizeof(double), nx*r_size, fp);
         fclose(fp);
-        fp=fopen(x_f, "wb");
+        fp=fopen("output/x.bin", "wb");
         fwrite(x, sizeof(double), nx, fp);
         fclose(fp);
     } 
 
     if (print_spectrum)
     {
+        printf("Printing spectrum.\n");
         fp=fopen("output/spectrum.bin", "wb");
-        fwrite(spectrum, sizeof(double), size*nt, fp);
+        fwrite(spectrum, sizeof(double), nt*s_size, fp);
         fclose(fp);
     }
 
     // Clean up
     printf("Cleaninig up.\n");
+    // Destroy plans
     fftw_destroy_plan(forward);
     fftw_destroy_plan(backward);
     if (print_spectrum)
         fftw_destroy_plan(f0_plan);
+
+    // Free base arrays
     fftw_free(psi);
+    free(x);
+    free(k2);
+    free(k);
+    
+    // Free conditional arrays
     if (print_spectrum)
     {
         free(spectrum);
         fftw_free(psi_f);
     }
-    if (print_results)
+    if (print_psi)
     {
         free(psi_i); 
         free(psi_r);
     }
-    free(x);
-    free(k2);
-    free(k);
-
     if (order > 2)
     {
         fftw_free(psi1);
